6635174fa0d3d64a7febed152e9dfceb6050d0c5
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2024-12-28 10:35:56 -0300

    Finalizado o modulo 1 mais um projeto onde estudei classes neste projeto criei um jogo de batalha em turnos onde o herói enfrenta um Ogro onde seus pontos de vida e nível são aleatórios, neste projeto utilizamos o conceito de classes e como eles são utilizado na estruturação do projeto.

---
+++ modulo_1/projeto_mod_1/jogo.py
@@ -0,0 +1,102 @@
+# O projeto consiste em um jogo de combate em turnos entre o nosso héroi e nosso inimigo 
+# onde a cada ataque normal ou especial causará dano nos pontos de vida do adversário 
+# o primeiro que zerar a vida perde
+
+# Personagem: classe mãe
+# Héroi: será controlado pelo usuário
+# Inimigo: adversário do usuário
+
+import random
+
+class Personagem:
+    def __init__(self, nome, vida, nivel):
+        self.__nome = nome
+        self.__vida = vida
+        self.__nivel= nivel
+
+    def get_nome(self):
+        return self.__nome
+    
+    def get_vida(self):
+        return self.__vida
+    
+    def get_nivel(self):
+        return self.__nivel
+    
+    def exibir_detalhes(self):
+        return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
+
+    def receber_ataque(self, dano):
+        self.__vida -= dano
+        if self.__vida <= 0:
+            self.__vida = 0 
+
+    def atacar(self, alvo):
+        dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4)
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano")
+
+class Heroi(Personagem):
+    def __init__(self, nome, vida, nivel, habilidade):
+        super().__init__(nome, vida, nivel)
+        self.__habilidade = habilidade
+
+    def get_habilidade(self):
+        return self.__habilidade
+    
+    def exibir_detalhes(self):
+        return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
+    
+    def ataque_especial(self, alvo):
+        dano = random.randint(self.get_nivel() * 5, self.get_nivel() * 8) # Dano Aumentado
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano")
+    
+
+class Inimigo(Personagem):
+    def __init__(self, nome, vida, nivel, tipo):
+        super().__init__(nome, vida, nivel)
+        self.__tipo = tipo
+
+    def get_tipo(self):
+        return self.__tipo
+
+    def exibir_detalhes(self):
+        return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}"
+
+# Classe orquestradora do jogo
+class Jogo:
+    def __init__(self):
+        self.heroi = Heroi(nome="Herói", vida = random.randint(50, 150), nivel= random.randint(2, 10), habilidade="Super Força")
+        self.inimigo = Inimigo(nome="Ogro", vida = random.randint(50, 150), nivel= random.randint(2, 10), tipo="Guerreiro")
+
+    # Fazer a gestão da batalha em turnos
+    def iniciar_batalha(self):
+        print("Iniciando a batalha!")
+        while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
+            print("\nDetalhes dos Personagens:")
+            print(self.heroi.exibir_detalhes())
+            print(self.inimigo.exibir_detalhes())
+
+            input("Pressione Enter para atacar...")
+            escolha = input("Escolha (1 - Ataque Normal, 2 - Ataque Especial): ")
+
+            if escolha == "1":
+                self.heroi.atacar(self.inimigo)
+            elif escolha == "2":
+                self.heroi.atacar(self.inimigo)
+            else:
+                print("Escolha inválida. Escolha novamente.")
+
+            # Inimigo ataca o héroi
+            if self.inimigo.get_vida() > 0:
+                self.inimigo.atacar(self.heroi)
+        
+        if self.heroi.get_vida() > 0:
+            print("\nParabéns, você venceu a batalha!")
+        else: 
+            print("\nVocê foi derrotado")
+
+# Criar instância do jogo e iniciar a batalha
+jogo = Jogo()
+jogo.iniciar_batalha()
