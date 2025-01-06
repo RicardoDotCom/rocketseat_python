fe8ecbcad35b575b2b9b138a6702f6d1038c9981
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-01-06 15:42:53 -0300

    Criado a rota para a inclusão de uma refeição

--- modulo_2_flask_api_autenticacao/desafio_mod_2_flask/app.py
+++ modulo_2_flask_api_autenticacao/desafio_mod_2_flask/app.py
@@ -4,6 +4,26 @@ from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/flask_crud_api_dieta"
 
 db.init_app(app)
+
+@app.route('/refeicao', methods=['POST'])
+def incluir_refeicao():
+    data = request.json
+    refeicao = data.get("refeicao")
+    descricao = data.get("descricao")
+    data_refeicao = data.get("data_refeicao")
+    esta_na_dieta = data.get("esta_na_dieta")
+
+    if refeicao and descricao and data_refeicao and esta_na_dieta:    
+        refeicao = Refeicao(refeicao=refeicao, descricao=descricao, data_refeicao=data_refeicao, esta_na_dieta=esta_na_dieta)
+        db.session.add(refeicao)
+        db.session.commit()
+        return jsonify({"message": "Refeição cadastrada com sucesso"})
+
+    return jsonify({"message": "Dados inválidos"}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- modulo_2_flask_api_autenticacao/desafio_mod_2_flask/models/refeicao.py
+++ modulo_2_flask_api_autenticacao/desafio_mod_2_flask/models/refeicao.py
@@ -1,9 +1,18 @@
 from database import db
 
-class User(db.Model):
+class Refeicao(db.Model):
     # id (int), username (text), password(text), role (text)
     id = db.Column(db.Integer, primary_key=True)
     refeicao = db.Column(db.String(80), nullable=False)
     descricao = db.Column(db.String(200), nullable=False)
     data_refeicao = db.Column(db.DateTime(), nullable=False)
-    dieta = db.Column(db.Boolean(), nullable=False, default=False)
+    esta_na_dieta = db.Column(db.Boolean(), nullable=False, default=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "refeicao": self.refeicao,
+            "descricao": self.descricao,
+            "data_refeicao": self.data_refeicao,
+            "esta_na_dieta":self.esta_na_dieta
+        }
