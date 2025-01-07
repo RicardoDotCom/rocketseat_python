3088e0c6721c2f4b5b0c1bad4360ed83420e068e
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-01-07 13:04:45 -0300

    Criação da classe Pix com a elaboração do ID e QR para testes

---
+++ modulo_3_flask_em_tempo_real/payments/pix.py
@@ -0,0 +1,20 @@
+import uuid
+import qrcode
+
+class Pix:
+    def __init__(self):
+        pass
+
+    def create_payment(self):
+        # Criar o pagamento na instituição financeira
+        bank_payment_id = uuid.uuid4()
+
+        # qr code - é uma informação, podendo ser texto/número, que foi gerada uma imagem
+        hash_payment = f'hash_payment_{bank_payment_id}'
+
+        # criação da imagem biblioteca QR CODE PYTHON 
+        img = qrcode.make(hash_payment)
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+
+        return {"bank_payment_id":bank_payment_id,
+                "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
