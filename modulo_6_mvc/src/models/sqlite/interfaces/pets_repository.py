5c152711f10436832d306147d922d17fe4073148
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-01-28 18:15:06 -0300

    feat: Implementing person_creator_controller with tests

--- modulo_6_mvc/src/models/sqlite/interfaces/pets_repository.py
+++ modulo_6_mvc/src/models/sqlite/interfaces/pets_repository.py
@@ -2,7 +2,7 @@ from typing import List
 from abc import ABC, abstractmethod
 from src.models.sqlite.entities.pets import PetsTable
 
-class PetsRepositoryInterface:
+class PetsRepositoryInterface(ABC):
 
     @abstractmethod
     def list_pets(self) -> List[PetsTable]:
