ef07f3b3cb2b4a958d834f3f2bac837bc43b84f5
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-02-14 08:16:40 -0300

    feat: Implenting BalanceEditorView and LoginCreatorView

--- modulo_7_autenticacao_JWT_seguranca/projeto_mod_7_JWT_seguranca/src/controllers/interfaces/balance_editor.py
+++ modulo_7_autenticacao_JWT_seguranca/projeto_mod_7_JWT_seguranca/src/controllers/interfaces/balance_editor.py
@@ -4,4 +4,4 @@ from abc import ABC, abstractmethod
 class BalanceEditorInterface(ABC):
 
     @abstractmethod
-    def editor(self, user_id: int, new_balance: float) -> Dict: pass
+    def edit(self, user_id: int, new_balance: float) -> Dict: pass
