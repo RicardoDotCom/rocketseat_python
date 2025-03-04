5566d2c78022f98c60bf0753675f33c4bbd9c802
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-03-04 16:49:36 -0300

    feat: Implementing http types

--- modulo_9_redis/projeto_modulo_9_redis/src/data/product_finder.py
+++ modulo_9_redis/projeto_modulo_9_redis/src/data/product_finder.py
@@ -1,7 +1,12 @@
 from src.models.sqlite.repository.interfaces.product_repository import ProductsRepositoryInterfaces
 from src.models.redis.repository.interfaces.redis_repository import RedisRepositoryInterfaces
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
 
 class ProductFinder:
     def __init__(self, redis_repo: RedisRepositoryInterfaces, product_repo: ProductsRepositoryInterfaces) -> None:
         self.__redis_repo = redis_repo
         self.__product_repo = product_repo
+
+    def find_by_name(self, http_request: HttpRequest) -> HttpResponse: pass 
+
